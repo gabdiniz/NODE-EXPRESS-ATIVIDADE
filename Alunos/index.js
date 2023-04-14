b06308ece5a62c046e4e8597025dc457b27383d8
Gabriel Diniz <gabrieldiniz.contato@gmail.com>
2023-04-14 17:18:00 -0300

    Rota POST para atualizar Aluno(a)

--- Alunos/index.js
+++ Alunos/index.js
@@ -6,24 +6,37 @@ app.use(express.json());
 
 app.get("/alunos", (req, res) => {
   const { nome, media } = req.query;
-  if (nome) res.json(findByNome(nome));
-  if (media) res.json(findByMedia(media));
+  if (nome) return res.json(findByNome(nome));
+  if (media) return res.json(findByMedia(media));
   res.json(alunos);
 });
 
 app.post("/alunos/novo", (req, res) => {
   const { nome, matricula, media } = req.body;
-  (!nome || !matricula || !media) ? res.status(400).json({ message: "Informações incorretas ou incompleta" }) : alunos.push({ nome: nome, matricula: matricula, media: media });
+  if (!nome || !matricula || !media) return res.status(400).json({ message: "Informações incorretas ou incompleta" })
+  alunos.push({ nome: nome, matricula: matricula, media: media });
   res.status(201).json({ message: "Aluno cadastrado" });
-})
+});
 
 app.post("/alunos/delete/:index", (req, res) => {
   const { index } = req.params;
-  if (!index) res.status(400).json({ message: "Insira um index" });
-  if (isNaN(index)) res.status(400).json({ message: "O index deve ser do tipo numérico" });
-  if (index >= alunos.length) res.status(404).json({ message: `Não existe aluno no index: ${index}` });
+  if (!index) return res.status(400).json({ message: "Insira um index" });
+  if (isNaN(index)) return res.status(400).json({ message: "O index deve ser do tipo numérico" });
+  if (index >= alunos.length) return res.status(404).json({ message: `Não existe aluno no index: ${index}` });
   alunos.splice(index, 1);
   res.status(201).json({ message: "Aluno removido" });
+});
+
+app.post("/alunos/atualizar/:index", (req, res) => {
+  const { index } = req.params;
+  const { nome, media } = req.body;
+  if (!index) return res.status(400).json({ message: "Insira um index" });
+  if (isNaN(index)) return res.status(400).json({ message: "O index deve ser do tipo numérico" });
+  if (!nome || !media) return res.status(400).json({ message: "Informações incorretas ou incompleta" });
+  if (index >= alunos.length) return res.status(404).json({ message: `Não existe aluno no index: ${index}` });
+  alunos[index] = ({ nome: nome, media: media });
+  res.status(201).json({ message: "Aluno atualizado" });
 })
+
 
 app.listen(3000, () => console.log("Servidor rodando em http://localhost:3000/"));
