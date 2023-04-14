d678d1b3ee3fbf8362abfbf7a3a92cf1e33a606e
Gabriel Diniz <gabrieldiniz.contato@gmail.com>
2023-04-14 10:41:37 -0300

    rota POST para deletar aluno(a)

--- Alunos/index.js
+++ Alunos/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { alunos, findByNome, findByMedia } = require("./alunos")
+const { alunos, findByNome, findByMedia } = require("./alunos");
 
 const app = express();
 app.use(express.json());
@@ -13,8 +13,17 @@ app.get("/alunos", (req, res) => {
 
 app.post("/alunos/novo", (req, res) => {
   const { nome, matricula, media } = req.body;
-  (!nome || !matricula || !media) ? res.status("404").json({ message: "Informações incorretas ou incompleta" }) : alunos.push({ nome: nome, matricula: matricula, media: media });
-  res.status("201").json({ message: "Aluno cadastrado" });
+  (!nome || !matricula || !media) ? res.status(400).json({ message: "Informações incorretas ou incompleta" }) : alunos.push({ nome: nome, matricula: matricula, media: media });
+  res.status(201).json({ message: "Aluno cadastrado" });
 })
 
-app.listen(3000, () => console.log("Servidor rodando em http://localhost:3000/"))
+app.post("/alunos/delete/:index", (req, res) => {
+  const { index } = req.params;
+  if (!index) res.status(400).json({ message: "Insira um index" });
+  if (isNaN(index)) res.status(400).json({ message: "O index deve ser do tipo numérico" });
+  if (index >= alunos.length) res.status(404).json({ message: `Não existe aluno no index: ${index}` });
+  alunos.splice(index, 1);
+  res.status(201).json({ message: "Aluno removido" });
+})
+
+app.listen(3000, () => console.log("Servidor rodando em http://localhost:3000/"));
