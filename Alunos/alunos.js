ade6b6c45b6fbc5267bc09ea7a5c9e1d8c57d80c
Gabriel Diniz <gabrieldiniz.contato@gmail.com>
2023-04-16 07:44:53 -0300

    Desafio 1: Mover para alunos.js, os métodos de deletar, adicionar e atualizar - Fix Desafio 0

--- Alunos/alunos.js
+++ Alunos/alunos.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 const alunos = [
   {
     nome: "Ruan",
@@ -21,6 +23,36 @@ const alunos = [
   },
 ]
 
+function addAluno(req, res) {
+  const { nome, matricula, media } = req.body;
+  if (!nome || !matricula || !media) return res.status(400).json({ message: "Informações incorretas ou incompleta" })
+  alunos.push({ nome: nome, matricula: matricula, media: media });
+  res.status(201).json({ message: "Aluno cadastrado" });
+  fs.writeFileSync("./db.json", JSON.stringify(alunos));
+}
+
+function deleteAluno(req, res) {
+  const { index } = req.params;
+  if (!index) return res.status(400).json({ message: "Insira um index" });
+  if (isNaN(index)) return res.status(400).json({ message: "O index deve ser do tipo numérico" });
+  if (index >= alunos.length) return res.status(404).json({ message: `Não existe aluno no index: ${index}` });
+  alunos.splice(index, 1);
+  fs.writeFileSync("./db.json", JSON.stringify(alunos));
+  res.status(201).json({ message: "Aluno removido" });
+}
+
+function attAluno(req, res) {
+  const { index } = req.params;
+  const { nome, media } = req.body;
+  if (!index) return res.status(400).json({ message: "Insira um index" });
+  if (isNaN(index)) return res.status(400).json({ message: "O index deve ser do tipo numérico" });
+  if (!nome || !media) return res.status(400).json({ message: "Informações incorretas ou incompleta" });
+  if (index >= alunos.length) return res.status(404).json({ message: `Não existe aluno no index: ${index}` });
+  alunos[index] = ({ nome: nome, media: media });
+  fs.writeFileSync("./db.json", JSON.stringify(alunos));
+  res.status(201).json({ message: "Aluno atualizado" });
+}
+
 function findByNome(nome) {
   if (nome) return alunos.filter((user) => user.nome.toLowerCase() == nome.toLowerCase());
 }
@@ -28,7 +60,7 @@ function findByMedia(media) {
   if (media) return alunos.filter((user) => user.media == media);
 }
 
-module.exports = { alunos, findByNome, findByMedia };
+module.exports = { alunos, findByNome, findByMedia, addAluno, deleteAluno, attAluno };
 
 
 
--- Alunos/index.js
+++ Alunos/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { alunos, findByNome, findByMedia } = require("./alunos");
+const { findByNome, findByMedia, addAluno, deleteAluno, attAluno } = require("./alunos");
 const fs = require("fs");
 
 const app = express();
@@ -9,38 +9,20 @@ app.get("/alunos", (req, res) => {
   const { nome, media } = req.query;
   if (nome) return res.json(findByNome(nome));
   if (media) return res.json(findByMedia(media));
-  res.json(alunos);
+  const alunos = fs.readFileSync('./db.json');
+  res.json(JSON.parse(alunos.toString()));
 });
 
 app.post("/alunos/novo", (req, res) => {
-  const { nome, matricula, media } = req.body;
-  if (!nome || !matricula || !media) return res.status(400).json({ message: "Informações incorretas ou incompleta" })
-  alunos.push({ nome: nome, matricula: matricula, media: media });
-  fs.writeFileSync("./db.json", JSON.stringify(alunos));
-  res.status(201).json({ message: "Aluno cadastrado" });
+  addAluno(req, res);
 });
 
 app.post("/alunos/delete/:index", (req, res) => {
-  const { index } = req.params;
-  if (!index) return res.status(400).json({ message: "Insira um index" });
-  if (isNaN(index)) return res.status(400).json({ message: "O index deve ser do tipo numérico" });
-  if (index >= alunos.length) return res.status(404).json({ message: `Não existe aluno no index: ${index}` });
-  alunos.splice(index, 1);
-  fs.writeFileSync("./db.json", JSON.stringify(alunos));
-  res.status(201).json({ message: "Aluno removido" });
+  deleteAluno(req, res);
 });
 
 app.post("/alunos/atualizar/:index", (req, res) => {
-  const { index } = req.params;
-  const { nome, media } = req.body;
-  if (!index) return res.status(400).json({ message: "Insira um index" });
-  if (isNaN(index)) return res.status(400).json({ message: "O index deve ser do tipo numérico" });
-  if (!nome || !media) return res.status(400).json({ message: "Informações incorretas ou incompleta" });
-  if (index >= alunos.length) return res.status(404).json({ message: `Não existe aluno no index: ${index}` });
-  alunos[index] = ({ nome: nome, media: media });
-  fs.writeFileSync("./db.json", JSON.stringify(alunos));
-  res.status(201).json({ message: "Aluno atualizado" });
+  attAluno(req, res);
 })
-
 
 app.listen(3000, () => console.log("Servidor rodando em http://localhost:3000/"));
